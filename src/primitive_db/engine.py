7dc8f7b97ae42a81dfb00545863161db4a2a8a95
Timofey Priezzhikh <seeneld@TimPC.>
2025-11-09 19:59:13 +0300

    Добавлена сторонняя библиотека prompt

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,19 @@
+import prompt
+
+"""
+Приветственное сообщение
+"""
+def welcome():
+    while True:
+        print('<command> exit - выйти из программы')
+        print('<command> help - справочная информация')
+        command = input('Введите команду: ')
+
+        if command == 'exit':
+            break
+
+        elif command == 'help':
+            continue
+        
+        else:
+            print(f"Неизвестная команда: {command}")
